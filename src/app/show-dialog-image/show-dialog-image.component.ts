a2fbb3c26cc6cf254baee3c7a2356ba68a623d09
Duc Minh Pham <duc.pham1@hitachivantara.com>
2022-12-19 09:28:27 +0700

    add lib

---
+++ src/app/show-dialog-image/show-dialog-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-dialog-image',
+  templateUrl: './show-dialog-image.component.html',
+  styleUrls: ['./show-dialog-image.component.scss']
+})
+export class ShowDialogImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
